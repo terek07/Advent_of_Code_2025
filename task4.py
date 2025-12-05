92d7955ca0411c906a101cfaa9111e866c0d0bd6
Tymoteusz Dawid <128647929+terek07@users.noreply.github.com>
2025-12-05 11:21:03 +0100

    day 4 part 2

--- task4.py
+++ task4.py
@@ -1,22 +1,32 @@
-with open("input4.txt") as file:
-    data = [e.strip() for e in file.readlines()]
+with open("input3.txt") as file:
+    data = [list(e.strip()) for e in file.readlines()]
 print(data)
 
-result = 0
-for x, row in enumerate(data):
-    for y, e in enumerate(row):
-        if e != "@":
-            continue
-        counter = 0
-        for px in range(-1, 2):
-            if x + px >= len(data) or x + px < 0:
-                continue
-            for py in range(-1, 2):
-                if y + py >= len(row) or y + py < 0:
-                    continue
-                if data[x + px][y + py] == "@":
-                   counter += 1
-        if counter < 5:
-            result += 1
-print(result)
+results = []
 
+def remove_avaible(data=data):
+    result = 0
+    removed = False
+    for x, row in enumerate(data):
+        for y, e in enumerate(row):
+            if e != "@":
+                continue
+            counter = 0
+            for px in range(-1, 2):
+                if x + px >= len(data) or x + px < 0:
+                    continue
+                for py in range(-1, 2):
+                    if y + py >= len(row) or y + py < 0:
+                        continue
+                    if data[x + px][y + py] == "@":
+                       counter += 1
+            if counter < 5:
+                result += 1
+                data[x][y] = '.'
+                removed = True
+    print(result)
+    results.append(result)
+    return removed
+while remove_avaible():
+    remove_avaible()
+print(sum(results))
