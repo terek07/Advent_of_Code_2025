1aeecf279bcdfa0a76644c5f77c3cbabf7d893d4
Tymoteusz Dawid <128647929+terek07@users.noreply.github.com>
2025-12-05 13:54:56 +0100

    day 5

--- task5.py
+++ task5.py
@@ -1,26 +1,32 @@
-from itertools import count
-
-with open("input3.txt") as file:
+with open("input5.txt") as file:
     data = file.read()
 data = data.split('\n\n')
-fresh = [[int(e) for e in e.split('-')] for e in data[0].split('\n')]
+fresh = [[int(e) for e in line.split('-')] for line in data[0].split('\n')]
 ingredients = [int(e) for e in data[1].split('\n')]
 # print(data)
 # print(fresh)
 # print(ingredients)
-
 counter = 0
-fresh.sort()
-print(fresh)
-norepeat_fresh = []
-for i, fresh_range in enumerate(fresh[1: ]):
-    if fresh_range[0] < fresh[i - 1][1]:
-        norepeat_fresh.append([min((fresh_range[0], fresh[i - 1][0])), min((fresh_range[1], fresh[i - 1][1]))])
-        #WIP
 for ingredient in ingredients:
     for fresh_range in fresh:
         if fresh_range[1] + 1 >= ingredient >= fresh_range[0]:
             counter += 1
             break
 print(counter)
-# print(len(all_fresh))
+
+# print(fresh)
+fresh.sort()
+# print(fresh)
+norepeat_fresh = [fresh[0]]
+for i, fresh_range in enumerate(fresh[1: ]):
+    if fresh_range[0] <= norepeat_fresh[-1][1]:
+        norepeat_fresh[-1] = ([min((fresh_range[0], norepeat_fresh[-1][0])), max((fresh_range[1], norepeat_fresh[-1][1]))])
+        continue
+    norepeat_fresh.append(fresh_range)
+
+# print(norepeat_fresh)
+
+counter2 = 0
+for e in norepeat_fresh:
+    counter2 += e[1] - e[0] + 1
+print(counter2)
